1e45d4f1e827365ae8d1ff11a1a8c923385f56b8
Bobxc <987257718@qq.com>
2021-04-01 16:24:59 +0800

    第一次提交

---
+++ src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <editorwang @change="changeWang"></editorwang> -->
+  <div style="width: 100%; height: 100%">
+    <el-button type="text" @click="dialogVisible = true"
+      >点击打开 Dialog</el-button
+    >
+    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+      <div>
+        <editorwang @change="changeWang"></editorwang>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import editorwang from "@/components/wangEnditor";
+export default {
+  name: "Home",
+  data() {
+    return {
+      dialogVisible: false,
+    };
+  },
+  components: {
+    editorwang,
+  },
+  methods: {
+    changeWang(html) {
+      console.log(html);
+    },
+  },
+
+  mounted() {},
+};
+</script>
+
+
+
